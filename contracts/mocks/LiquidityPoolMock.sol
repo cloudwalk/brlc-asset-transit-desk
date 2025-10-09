9ae652ed40eaaf1a948a81b61058ae65a10a0bca
Ilia Groshev <ilia.groshev@cloudwalk.io>
2025-10-09 12:51:53 +0100

    move mock contract

--- contracts/mocks/LiquidityPoolMock.sol
+++ contracts/mocks/LiquidityPoolMock.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.24;
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { ILiquidityPool } from "../../interfaces/ILiquidityPool.sol";
-import { AccessControlExtUpgradeable } from "../../base/AccessControlExtUpgradeable.sol";
+import { ILiquidityPool } from "../interfaces/ILiquidityPool.sol";
+import { AccessControlExtUpgradeable } from "../base/AccessControlExtUpgradeable.sol";
 
 /**
  * @title LiquidityPoolMock contract
