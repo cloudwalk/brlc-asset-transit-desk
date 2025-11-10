f88ec14320a54af5d77c6c007da0bac820a03630
Ilia Groshev <ilia.groshev@cloudwalk.io>
2025-11-10 15:01:20 +0000

    feat: rollback oz to 5.4. Readme

--- contracts/base/UUPSExtUpgradeable.sol
+++ contracts/base/UUPSExtUpgradeable.sol
@@ -2,15 +2,14 @@
 
 pragma solidity ^0.8.24;
 
-import { UUPSUpgradeable } from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
-import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 /**
  * @title UUPSExtUpgradeable base contract
  * @author CloudWalk Inc. (See https://www.cloudwalk.io)
  * @dev Extends OpenZeppelin's {UUPSUpgradeable} contract with additional checks for the new implementation address.
  */
-abstract contract UUPSExtUpgradeable is Initializable, UUPSUpgradeable {
+abstract contract UUPSExtUpgradeable is UUPSUpgradeable {
     // ------------------ Errors ---------------------------------- //
 
     /// @dev Thrown if the provided new implementation address is not a contract.
